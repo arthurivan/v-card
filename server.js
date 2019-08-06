41fe03241e8117ce8194507b0e151ac37a128a87
arthurivan <arthurivancowdery@gmail.com>
2019-08-06 19:13:31 +0300

    added procfile and changed port

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const PORT = process.env.PORT || 3000;
+
 const express = require('express');
 
 const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
-const server = app.listen(3000);
+const server = app.listen(PORT);
